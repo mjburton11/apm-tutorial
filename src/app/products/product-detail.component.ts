abf5a512abec199eef61c9c755d2244dd895406e
Michael Burton <mjburton11@Michaels-MacBook-Pro.local>
2021-04-26 09:55:18 -0400

    more routes

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { IProduct } from "./product";
 
 @Component({
-  templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  templateUrl: "./product-detail.component.html",
+  styleUrls: ["./product-detail.component.css"],
 })
 export class ProductDetailComponent implements OnInit {
-  pageTitle: string = 'Product Detail';
+  pageTitle: string = "Product Detail";
+  product: IProduct | undefined = {
+    productId: 5,
+    productName: "Hammer",
+    productCode: "TBX-0048",
+    releaseDate: "May 21, 2021",
+    description: "Curved claw steel hammer",
+    price: 8.9,
+    starRating: 4.8,
+    imageUrl: "assets/images/hammer.png",
+  };
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+    private router: Router) {}
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get("id"));
+    this.pageTitle += `: ${id}`;
   }
 
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
